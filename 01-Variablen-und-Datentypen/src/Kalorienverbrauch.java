9252044474f9215dc053863753bba855b5d86106
MekiAnjo <39812088+MekiAnjo@users.noreply.github.com>
2021-07-25 17:15:35 +0200

    Aufgabe 2 fertig

--- 01-Variablen-und-Datentypen/src/Kalorienverbrauch.java
+++ 01-Variablen-und-Datentypen/src/Kalorienverbrauch.java
@@ -5,16 +5,38 @@ import java.util.Scanner;
  */
 public class Kalorienverbrauch {
 
-  private static final Scanner EINGABE = new Scanner(System.in);
-
   public static void main(String[] args) {
+    Scanner eingabe = new Scanner(System.in);
+
     System.out.print("Gib dein Körpergewicht in KG ein: ");
-    double gewicht  = EINGABE.nextDouble();
+    double gewicht = eingabeEinlesen(eingabe);
     System.out.print("Gib die gelaufene oder zu laufende Distanz in km an: ");
-    double distanz = EINGABE.nextDouble();
-    double energieumsatz = gewicht * distanz;
+    double distanz = eingabeEinlesen(eingabe);
+    double energieumsatz = berechneKcal(gewicht, distanz);
     System.out.println();
+    printOutput(gewicht, distanz, energieumsatz);
+  }
+
+  private static double eingabeEinlesen(Scanner eingabe) {
+    return eingabe.nextDouble();
+  }
+
+  private static double berechneKcal(double gewicht, double distanz) {
+    return gewicht * distanz;
+  }
+
+  private static void printOutput(
+    double gewicht,
+    double distanz,
+    double energieumsatz
+  ) {
     System.out.println("Berechnung:");
-    System.out.printf("%.2f kg x %.2f km = %.2f kcal", gewicht, distanz, energieumsatz);
+
+    System.out.printf(
+      "%.2f kg x %.2f km = %.2f kcal",
+      gewicht,
+      distanz,
+      energieumsatz
+    );
   }
 }

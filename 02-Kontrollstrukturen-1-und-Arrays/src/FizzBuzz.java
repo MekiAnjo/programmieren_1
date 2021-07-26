e98d24f1b26528bb9f94281f4dfa37e46fa89d8f
MekiAnjo <39812088+MekiAnjo@users.noreply.github.com>
2021-07-27 01:49:41 +0200

    02-Kontrollstrukturen-1-und-Arrays/src/FizzBuzz: made it better credits vscode yt

--- 02-Kontrollstrukturen-1-und-Arrays/src/FizzBuzz.java
+++ 02-Kontrollstrukturen-1-und-Arrays/src/FizzBuzz.java
@@ -4,20 +4,31 @@
 public class FizzBuzz {
 
   public static void main(String[] args) {
+    // for (int i = 1; i <= 100; i++) {
+    //   if (i % 5 == 0 && i % 7 == 0) {
+    //     System.out.println("FizzBuzz");
+    //     continue;
+    //   }
+    //   if (i % 5 == 0) {
+    //     System.out.println("Fizz");
+    //     continue;
+    //   }
+    //   if (i % 7 == 0) {
+    //     System.out.println("Buzz");
+    //     continue;
+    //   }
+    //   System.out.println(i);
+    // }
+
+    fizzBuzz();
+  }
+
+  private static void fizzBuzz() {
     for (int i = 1; i <= 100; i++) {
-      if (i % 5 == 0 && i % 7 == 0) {
-        System.out.println("FizzBuzz");
-        continue;
-      }
-      if (i % 5 == 0) {
-        System.out.println("Fizz");
-        continue;
-      }
-      if (i % 7 == 0) {
-        System.out.println("Buzz");
-        continue;
-      }
-      System.out.println(i);
+      String s = "";
+      if (i % 5 == 0) s = "Fizz";
+      if (i % 7 == 0) s += "Buzz";
+      System.out.println(s.equals("") ? i : s);
     }
   }
 }

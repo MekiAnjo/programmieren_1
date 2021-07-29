aec499b607367c33d503c9c2a443000c7c37bbc9
MekiAnjo <39812088+MekiAnjo@users.noreply.github.com>
2021-07-29 19:42:19 +0200

    07-Interfaces/src/aufgabe3: fertig

---
+++ 07-Interfaces/src/aufgabe3/Main.java
@@ -0,0 +1,47 @@
+package aufgabe3;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Student[] students = new Student[6];
+
+    students[0] = new Student("Tony Khoa Nam Huynh", 1003166, "Informatik");
+    students[1] = new Student("Daniel Kom", 4596251, "Informatik");
+    students[2] = new Student("Emre Gezer", 6634304, "Informatik");
+    students[3] = new Student("Kevin Tao", 7390357, "Informatik");
+    students[4] = new Student("Robert Khoa Van Huynh", 4425492, "Biologie");
+    students[5] = new Student("Alex Nekrasov", 6766531, "Mathematik");
+
+    System.out.println(Arrays.toString(students));
+
+    Arrays.sort(students, new Student.ByMatrikelNr());
+
+    System.out.println();
+    System.out.println("byMatrikelnummer: ");
+    System.out.println(Arrays.toString(students));
+
+    Arrays.sort(students, new Student.ByName());
+
+    System.out.println();
+    System.out.println("byName: ");
+    System.out.println(Arrays.toString(students));
+
+    Arrays.sort(
+      students,
+      new Comparator() {
+        @Override
+        public int compare(Object o1, Object o2) {
+          return ((Student) o1).getStudienfach()
+            .compareTo(((Student) o2).getStudienfach());
+        }
+      }
+    );
+
+    System.out.println();
+    System.out.println("byStudiengang: ");
+    System.out.println(Arrays.toString(students));
+  }
+}

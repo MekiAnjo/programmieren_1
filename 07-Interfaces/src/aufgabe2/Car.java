d87051da020194c1a76ded996bf4373d8a1eafa5
MekiAnjo <39812088+MekiAnjo@users.noreply.github.com>
2021-07-29 19:10:52 +0200

    07-Interfaces/src/aufgabe2: fertig

---
+++ 07-Interfaces/src/aufgabe2/Car.java
@@ -0,0 +1,6 @@
+package aufgabe2;
+
+public interface Car {
+  void drive(int x, int y);
+  void printPosition();
+}
---
+++ 07-Interfaces/src/aufgabe2/Tesla.java
@@ -0,0 +1,29 @@
+package aufgabe2;
+
+public class Tesla implements Car {
+
+  private String model;
+  private int horsepower;
+  private int xPosition;
+  private int yPosition;
+
+  public Tesla(String model, int horsepower, int xPosition, int yPosition) {
+    this.model = model;
+    this.horsepower = horsepower;
+    this.xPosition = xPosition;
+    this.yPosition = yPosition;
+  }
+
+  @Override
+  public void drive(int x, int y) {
+    xPosition += x;
+    yPosition += y;
+    System.out.println("das Auto hat sich erfolgreich bewegt!");
+  }
+
+  @Override
+  public void printPosition() {
+    System.out.println("Aktuelle x-Position: " + xPosition);
+    System.out.println("Aktuelle y-Position: " + yPosition + "\n");
+  }
+}

edcf42e41d152d382f1e72387f767b2758a2a88e
MekiAnjo <39812088+MekiAnjo@users.noreply.github.com>
2021-08-01 23:01:35 +0200

    Variablen fertig

---
+++ 02-Variablen/src/de/hft_stuttgart/huto1bif/Variablen.java
@@ -0,0 +1,24 @@
+package de.hft_stuttgart.huto1bif;
+
+public class Variablen {
+
+  public static void main(String[] args) {
+    byte meinByte = 0;
+    short meinShort = 0;
+    int meinInt = 0;
+    long meinLong = 0;
+    float meinFloat = 0.0f;
+    double meinDouble = 0.0;
+    char meinChar = 'a';
+    boolean meinBool = true;
+
+    System.out.println(meinByte);
+    System.out.println(meinShort);
+    System.out.println(meinInt);
+    System.out.println(meinLong);
+    System.out.println(meinFloat);
+    System.out.println(meinDouble);
+    System.out.println(meinChar);
+    System.out.println(meinBool);
+  }
+}

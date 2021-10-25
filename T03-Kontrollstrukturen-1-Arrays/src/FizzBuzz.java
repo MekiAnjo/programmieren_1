eb1cef449b9ab52f8f03d05492b75cdfe51697c8
MekiAnjo <39812088+MekiAnjo@users.noreply.github.com>
2021-10-25 19:36:24 +0200

    added T03 Projekt

---
+++ T03-Kontrollstrukturen-1-Arrays/src/FizzBuzz.java
@@ -0,0 +1,11 @@
+public class FizzBuzz {
+
+  public static void main(String[] args) {
+    for (int i = 1; i <= 100; i++) {
+      String output = "";
+      if (i % 5 == 0) output = "Fizz";
+      if (i % 7 == 0) output += "Buzz";
+      System.out.println(output.isEmpty() ? i : output);
+    }
+  }
+}

4aec0706eb2215184614672ecc3835df23d23037
MekiAnjo <39812088+MekiAnjo@users.noreply.github.com>
2021-11-01 22:46:51 +0100

    made recursion a bit shorter

--- 04-Kontrollstrukturen-Rekursion-Zeichenketten/src/de/hft_stuttgart/huto1bif/rekursion/BinomialkoeffizentIterativ.java
+++ 04-Kontrollstrukturen-Rekursion-Zeichenketten/src/de/hft_stuttgart/huto1bif/rekursion/BinomialkoeffizentIterativ.java
@@ -22,8 +22,7 @@ public class BinomialkoeffizentIterativ {
   }
 
   private static int fak(int n) {
-    if (n == 0) return 1;
-    if (n == 1) return 1;
+    if (n == 0 || n == 1) return 1;
     return n * fak(n - 1);
   }
 }

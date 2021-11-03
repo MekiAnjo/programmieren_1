bc8a2aa57d24744443ee003c67e924c0dbd39d91
MekiAnjo <39812088+MekiAnjo@users.noreply.github.com>
2021-11-03 20:46:46 +0100

    Bubblesort done with optimized version

--- 05-Objektorientierung-Kapselung/src/de/hft_stuttgart/huto1bif/aufgabe5_3_bubblesort/MySort_Optimized.java
+++ 05-Objektorientierung-Kapselung/src/de/hft_stuttgart/huto1bif/aufgabe5_3_bubblesort/MySort_Optimized.java
@@ -2,16 +2,20 @@ package de.hft_stuttgart.huto1bif.aufgabe5_3_bubblesort;
 
 import java.util.Arrays;
 
-public class MySort {
+public class MySort_Optimized {
 
   public static void main(String[] args) {
     int[] intArray = new int[] { 5, 90, 35, 150, 45, 3 };
     int[] intArray2 = new int[] { 3, 5, 35, 45, 90, 150 };
+    int[] intArray3 = new int[] { 5, 3, 35, 45, 90, 150 };
     sort(intArray);
     System.out.println(Arrays.toString(intArray));
     System.out.println();
     sort(intArray2);
     System.out.println(Arrays.toString(intArray2));
+    System.out.println();
+    sort(intArray3);
+    System.out.println(Arrays.toString(intArray3));
     System.out.println();
   }
 
@@ -19,18 +23,21 @@ public class MySort {
     // … sortieren und dann auf der Konsole ausgeben
     int cycles = 0;
     int shiftCount = 0;
-    for (int i = 0; i < intArray.length; i++) {
-      for (int j = 0; j < intArray.length - 1; j++) {
+    for (int i = 0; i < intArray.length - 1; i++) {
+      boolean isSwapped = false;
+      for (int j = 0; j < intArray.length - 1 - i; j++) {
         int currentElement = intArray[j];
         int nextElement = intArray[j + 1];
 
         if (currentElement > nextElement) {
           intArray[j] = nextElement;
           intArray[j + 1] = currentElement;
+          isSwapped = true;
           shiftCount++;
         }
       }
       cycles++;
+      if (!isSwapped) break;
     }
     if (shiftCount == 0) {
       System.out.println("Sortierung unnötig!");

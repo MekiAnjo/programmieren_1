6e7edd4b38fba14f82a7367b7b10a712f497cdbe
MekiAnjo <39812088+MekiAnjo@users.noreply.github.com>
2021-11-30 14:47:04 +0100

    comparator anonym

---
+++ 08-Interfaces-Abstrakte-Klassen-verschachtelte-Klassen/src/de/hft_stuttgart/huto1bif/a2y_studentenVergleichAnonym/Student.java
@@ -0,0 +1,70 @@
+package de.hft_stuttgart.huto1bif.a2y_studentenVergleichAnonym;
+
+import java.util.Comparator;
+
+public class Student {
+
+  private String name;
+  private String vorname;
+  private String studium;
+  private int matrikel;
+
+  public Student(String name, String vorname, String studium, int matrikel) {
+    this.name = name;
+    this.vorname = vorname;
+    this.studium = studium;
+    this.matrikel = matrikel;
+  }
+
+  @Override
+  public String toString() {
+    return (
+      "Student{" +
+      "name=" +
+      name +
+      ", vorname=" +
+      vorname +
+      ", studium=" +
+      studium +
+      ", matrikel=" +
+      matrikel +
+      '}'
+    );
+  }
+
+  public static Comparator getCompareByNachname() {
+    return new Comparator() {
+      @Override
+      public int compare(Object o1, Object o2) {
+        return ((Student) o1).name.compareTo(((Student) o2).name);
+      }
+    };
+  }
+
+  public static Comparator getCompareByVorname() {
+    return new Comparator() {
+      @Override
+      public int compare(Object o1, Object o2) {
+        return ((Student) o1).vorname.compareTo(((Student) o2).vorname);
+      }
+    };
+  }
+
+  public static Comparator getCompareByStudium() {
+    return new Comparator() {
+      @Override
+      public int compare(Object o1, Object o2) {
+        return ((Student) o1).studium.compareTo(((Student) o2).studium);
+      }
+    };
+  }
+
+  public static Comparator getCompareByMatrikel() {
+    return new Comparator() {
+      @Override
+      public int compare(Object o1, Object o2) {
+        return ((Student) o1).matrikel - ((Student) o2).matrikel;
+      }
+    };
+  }
+}
---
+++ 08-Interfaces-Abstrakte-Klassen-verschachtelte-Klassen/src/de/hft_stuttgart/huto1bif/a2y_studentenVergleichAnonym/Studentenvergleich.java
@@ -0,0 +1,48 @@
+package de.hft_stuttgart.huto1bif.a2y_studentenVergleichAnonym;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Studentenvergleich {
+
+  public static void main(String[] args) {
+    Student studenten[] = new Student[6];
+    studenten[0] = new Student("Baur", "Melanie", "INF", 42);
+    studenten[1] = new Student("Speiser", "Sebastian", "INF", 43);
+    studenten[2] = new Student("Rausch", "Alexander", "WINF", 44);
+    studenten[3] = new Student("Pado", "Ulrike", "WINF", 41);
+    studenten[4] = new Student("Knauth", "Stefan", "INF", 48);
+    studenten[5] = new Student("Mosler", "Christof", "WINF", 47);
+
+    System.out.println("Wählen Sie ...");
+    System.out.println("1 - Nachname");
+    System.out.println("2 - Vorname");
+    System.out.println("3 - Studiengang");
+    System.out.println("4 - Matrikelnummer");
+    System.out.println("X - Programmende");
+
+    System.out.print("Ihre Eingabe: ");
+    Scanner scanner = new Scanner(System.in);
+    Character c = scanner.next().charAt(0);
+    System.out.println("c = " + c);
+
+    Comparator comparator =
+      switch (c) {
+        case '1' -> Student.getCompareByNachname();
+        case '2' -> Student.getCompareByVorname();
+        case '3' -> Student.getCompareByStudium();
+        case '4' -> Student.getCompareByMatrikel();
+        case 'X' -> null;
+        default -> null;
+      };
+
+    if (comparator != null) {
+      Arrays.sort(studenten, comparator);
+    }
+
+    for (Student s : studenten) {
+      System.out.println(s);
+    }
+  }
+}

c51813df7c391b78415edfebc91cdc74f59794d0
MekiAnjo <39812088+MekiAnjo@users.noreply.github.com>
2021-12-03 15:01:18 +0100

    initial commit for task

---
+++ 10-Enums-innere-Klassen-Exceptions/src/u1_verschachtelteKlassengetrennteKlassen/Main.java
@@ -0,0 +1,9 @@
+package u1_verschachtelteKlassengetrennteKlassen;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Spieler spieler = new Spieler("Max");
+    System.out.println(spieler.calculate());
+  }
+}
---
+++ 10-Enums-innere-Klassen-Exceptions/src/u1_verschachtelteKlassengetrennteKlassen/Wuerfel.java
@@ -0,0 +1,8 @@
+package u1_verschachtelteKlassengetrennteKlassen;
+
+public class Wuerfel {
+
+  public int calculate() {
+    return (int) (Math.random() * 6) + 1;
+  }
+}

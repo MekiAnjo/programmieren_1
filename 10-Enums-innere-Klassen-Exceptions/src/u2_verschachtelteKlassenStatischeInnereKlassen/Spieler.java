b33ba253455620a932d6eca6c1dba73322805c2b
MekiAnjo <39812088+MekiAnjo@users.noreply.github.com>
2021-12-03 15:15:31 +0100

    moved code from wuerfel into player class

--- 10-Enums-innere-Klassen-Exceptions/src/u2_verschachtelteKlassenStatischeInnereKlassen/Spieler.java
+++ 10-Enums-innere-Klassen-Exceptions/src/u2_verschachtelteKlassenStatischeInnereKlassen/Spieler.java
@@ -12,4 +12,11 @@ public class Spieler {
     Wuerfel wuerfel = new Wuerfel();
     return name + ": " + wuerfel.calculate();
   }
+
+  private static class Wuerfel {
+
+    public int calculate() {
+      return (int) (Math.random() * 6) + 1;
+    }
+  }
 }
--- 10-Enums-innere-Klassen-Exceptions/src/u2_verschachtelteKlassenStatischeInnereKlassen/Wuerfel.java
+++ 10-Enums-innere-Klassen-Exceptions/src/u2_verschachtelteKlassenStatischeInnereKlassen/Wuerfel.java
@@ -1,8 +1,8 @@
-package u2_verschachtelteKlassenStatischeInnereKlassen;
+// package u2_verschachtelteKlassenStatischeInnereKlassen;
 
-public class Wuerfel {
+// public class Wuerfel {
 
-  public int calculate() {
-    return (int) (Math.random() * 6) + 1;
-  }
-}
+//   public int calculate() {
+//     return (int) (Math.random() * 6) + 1;
+//   }
+// }

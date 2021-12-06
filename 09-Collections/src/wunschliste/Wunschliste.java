49af2edbada2991a35c075ba203131c44c519765
MekiAnjo <39812088+MekiAnjo@users.noreply.github.com>
2021-12-06 19:34:17 +0100

    added shorter add method

--- 09-Collections/src/wunschliste/Wunschliste.java
+++ 09-Collections/src/wunschliste/Wunschliste.java
@@ -1,12 +1,16 @@
 package wunschliste;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Wunschliste extends ArrayList<String> {
 
-  public void add(String... str) {
-    for (String string : str) {
-      add(string);
-    }
+  // public void add(String... elements) {
+  //   for (String string : elements) {
+  //     add(string);
+  //   }
+  // }
+  public void add(String... elements) {
+    addAll(Arrays.asList(elements));
   }
 }

7bdb98c4867cf954400e99a8a22713fe8b9a34f7
MekiAnjo <39812088+MekiAnjo@users.noreply.github.com>
2021-12-12 18:26:54 +0100

    fixed input file path

--- 10-Dateiverarbeitung/src/de/hft_stuttgart/huto1bif/csv2txt/Main.java
+++ 10-Dateiverarbeitung/src/de/hft_stuttgart/huto1bif/csv2txt/Main.java
@@ -5,10 +5,20 @@ import java.io.File;
 public class Main {
 
   public static void main(String[] args) {
-    KundenCSV2Aufkleber konvertierer = new KundenCSV2AufkleberIO();
-    //KundenCSV2Aufkleber konvertierer = new KundenCSV2AufkleberNIO();
+    // KundenCSV2Aufkleber konvertierer = new KundenCSV2AufkleberIO();
+    KundenCSV2Aufkleber konvertierer = new KundenCSV2AufkleberNIO();
+    // konvertierer.konvertiereKundendaten(
+    //   new File("kundendaten.csv"),
+    //   new File("kundendaten.txt")
+    // );
+
+    // Zum herausfinden wo der root folder ist
+    // File file = new File(".");
+    // for (String fileNames : file.list()) System.out.println(fileNames);
     konvertierer.konvertiereKundendaten(
-      new File("kundendaten.csv"),
+      new File(
+        "10-Dateiverarbeitung\\src\\de\\hft_stuttgart\\huto1bif\\csv2txt\\kundendaten.csv"
+      ),
       new File("kundendaten.txt")
     );
   }

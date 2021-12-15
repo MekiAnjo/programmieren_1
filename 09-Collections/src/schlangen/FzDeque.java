bf429c47bff3c9885364b980108c8137d92244c2
MekiAnjo <39812088+MekiAnjo@users.noreply.github.com>
2021-12-15 03:19:47 +0100

    added another method

--- 09-Collections/src/schlangen/FzDeque.java
+++ 09-Collections/src/schlangen/FzDeque.java
@@ -1,10 +1,14 @@
 package schlangen;
 
 import java.util.ArrayDeque;
+import java.util.Arrays;
 
 public class FzDeque extends ArrayDeque<String> {
 
   public FzDeque(String... string) {
+    // kürzere Variante
+    // Arrays.asList(string);
+
     for (String s : string) {
       super.add(s);
     }
